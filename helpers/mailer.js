57aa54e99fdc581fc56c456eb0b0454cfda0451b
chihabeddine <chihabeddine@chihabeddines-MacBook-Pro.local>
2022-08-02 06:34:30 +0100

    display post of followers, save,unsave, download image, delete post

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -6,7 +6,6 @@ const { OAuth2 } = google.auth;
 const oauth_link = "https://developers.google.com/oauthplayground";
 const { EMAIL, MAILING_ID, MAILING_REFRESH, MAILING_SECRET } = process.env;
 
-
 const auth = new OAuth2(
   MAILING_ID,
   MAILING_SECRET,
